1184e941f486729ccaf8f7028a34d610206a6f59
김주원 <kjw11077@naver.com>
2020-10-28 19:28:56 +0900

    update binary_tree_array

--- binary_tree_array.py
+++ binary_tree_array.py
@@ -12,9 +12,10 @@ class BinaryTree:
                 return
 
             # 노드 순회
-            print(self.array[i])
-            # left, right 노드 순회
+            print(self.array[i], end=' ')
+            # left 순회
             traversal(i * 2 + 1)
+            # right 순회
             traversal(i * 2 + 2)
             return
 
@@ -30,7 +31,7 @@ class BinaryTree:
             # left 순회
             traversal(i * 2 + 1)
             # 노드 순회
-            print(self.array[i])
+            print(self.array[i], end=' ')
             # right 순회
             traversal(i * 2 + 2)
 
@@ -48,7 +49,7 @@ class BinaryTree:
             # right 순회
             traversal(i * 2 + 2)
             # 노드 순회
-            print(self.array[i])
+            print(self.array[i], end=' ')
 
         traversal(0)
         return
@@ -60,7 +61,7 @@ class BinaryTree:
         return False
 
     def dfs(self, value):
-        def traversal(i):
+        def search(i):
 
             # 노드가 없다면 리턴
             if len(self.array) <= i:
@@ -71,17 +72,23 @@ class BinaryTree:
                 return True
 
             # left, right 노드 탐색
-            if traversal(i * 2 + 1) or traversal(i * 2 + 2):
+            if search(i * 2 + 1) or search(i * 2 + 2):
                 return True
             else:
                 return False
 
-        return traversal(0)
+        return search(0)
 
 
 bt = BinaryTree([i for i in range(1, 11)])
-# bt.preorder()  # 1 2 4 8 9 5 10 3 6 7
-# bt.inorder()  # 8 4 9 2 10 5 1 6 3 7
-# bt.postorder()  # 8 9 4 10 5 2 6 7 3 1
-# print(bt.bfs(1))
-print(bt.dfs(1))
+print('preorder')
+bt.preorder()  # 1 2 4 8 9 5 10 3 6 7
+print()
+print('inorder')
+bt.inorder()  # 8 4 9 2 10 5 1 6 3 7
+print()
+print('postorder')
+bt.postorder()  # 8 9 4 10 5 2 6 7 3 1
+print()
+print('bfs', bt.bfs(1))
+print('dfs', bt.dfs(1))
