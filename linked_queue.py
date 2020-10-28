7e0169bfb5fa08e0a8b91b5fe0a13f1a3b9e0685
김주원 <kjw11077@naver.com>
2020-10-28 16:28:43 +0900

    implement linked_queue

---
+++ linked_queue.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+
+class LinkedQueue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def put(self, value):
+        if self.is_empty():
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            node = Node(value, self.tail, None)
+            self.tail.next = node
+            self.tail = node
+
+    def get(self):
+        # 언더플로우
+        if self.is_empty():
+            return None
+
+        value = self.head.value
+        self.head = self.head.next
+        self.head.prev = None
+        return value
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.head.value
+
+    def print(self):
+        pass
+
+    def is_empty(self):
+        return self.head is None
